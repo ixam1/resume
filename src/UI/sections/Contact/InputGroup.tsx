1a8d2756ca2d438af81d315327d9598bd7c99b83
Maximilian Schaum <max.schaum4@gmail.com>
2022-03-21 14:27:11 +0100

    :bug: fix form input names

--- src/UI/sections/Contact/InputGroup.tsx
+++ src/UI/sections/Contact/InputGroup.tsx
@@ -15,6 +15,7 @@ function InputGroup({ type = "text", ...props }: Props) {
 			<Element
 				type={type === "textarea" ? undefined : type}
 				rows={props.rows}
+				name={props.name}
 				required={props.required}
 				placeholder={props.name + (props.required ? "*" : "")}
 				className="block w-full pt-4 pb-3 placeholder-accent-200 border-0 border-b border-white rounded bg-accent-700 hover:bg-accent-600 focus:border-primary focus:bg-accent-600 focus:ring-0"
