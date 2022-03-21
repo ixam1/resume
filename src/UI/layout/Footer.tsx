ec4b5c157c1b9b1c43bd1bf3b020cd53f2b44eb1
Maximilian Schaum <max.schaum4@gmail.com>
2022-03-21 01:30:38 +0100

    :sparkles: added compliance pages

--- src/UI/layout/Footer.tsx
+++ src/UI/layout/Footer.tsx
@@ -5,11 +5,15 @@ import AllSocialButtons from "../components/AllSocialButtons"
 function Footer() {
 	return (
 		<footer className="w-full bg-accent text-white py-5">
-			<div className="sectionwidth flex items-center justify-between">
+			<div className="sectionwidth flex items-center justify-between flex-wrap gap-4">
 				<div className="text-sm">
 					Copyright © 2022 Maximilian Schaum -{" "}
 					<Link to="/impressum" className="hover:underline">
 						Impressum
+					</Link>{" "}
+					-{" "}
+					<Link to="/datenschutz" className="hover:underline">
+						Datenschutz
 					</Link>
 				</div>
 				<AllSocialButtons />
---
+++ src/pages/impressum.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Section from "../UI/components/Section"
+import Layout from "../UI/layout/Layout"
+
+function impressum() {
+	return (
+		<Layout>
+			<Section fullscreen className="bg-accent">
+				<div className="prose prose-invert">
+					<h1>Impressum</h1>
+					<h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+					<p>
+						Maximilian Schaum
+						<br />
+						Alfred-D&ouml;blin-Allee 12
+						<br />
+						12529 Sch&ouml;nfeld
+					</p>
+					<h2>Kontakt</h2>
+					<p>
+						Telefon: 01622715832
+						<br />
+						E-Mail: max.schaum4@gmail.com
+					</p>
+				</div>
+			</Section>
+		</Layout>
+	)
+}
+
+export default impressum
