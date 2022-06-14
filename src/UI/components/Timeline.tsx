973d432b522690b29f420d00981cab17d503cd85
Maximilian Schaum <max.schaum4@gmail.com>
2022-06-14 15:51:08 +0200

    :sparkles: added link to timeline

--- src/UI/components/Timeline.tsx
+++ src/UI/components/Timeline.tsx
@@ -1,3 +1,4 @@
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
 import { faAnglesDown, faCircle } from "@fortawesome/pro-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import clsx from "clsx"
@@ -7,6 +8,10 @@ import useCollapse from "react-collapsed"
 type TimelineItemList = {
 	description: string
 	technologies?: string[]
+	link?: {
+		href: string
+		text: string
+	}
 }
 
 type TimelineItem = {
@@ -23,7 +28,7 @@ type TimelineItem = {
 
 function Timeline(props: { timeline: TimelineItem[] }) {
 	return (
-		<ul className="text-white w-full">
+		<ul className="w-full text-white">
 			{props.timeline.map((timelineItem, index) => {
 				const isOdd = index % 2 !== 0
 
@@ -60,17 +65,17 @@ function TimelineItem({ isOdd, item }: { isOdd: boolean; item: TimelineItem }) {
 			</div>
 			<div className="flex flex-col items-center gap-4 pt-1">
 				<FontAwesomeIcon icon={faCircle} size="sm" className="text-primary" />
-				<div className="flex-grow bg-white w-px"></div>
+				<div className="flex-grow w-px bg-white"></div>
 			</div>
 			<div className={clsx("flex-1 px-2 pb-16", isOdd && "lg:order-first")}>
-				<div className="block lg:hidden text-accent-400 text-sm mb-1">
+				<div className="block mb-1 text-sm lg:hidden text-accent-400">
 					{item.time}
 				</div>
 				<h2 className="mb-1 font-bold">{item.title}</h2>
 				<h3 className="text-primary">{item.subtitle}</h3>
 
 				{item.info && (
-					<small className="text-accent-400 italic">{item.info}</small>
+					<small className="italic text-accent-400">{item.info}</small>
 				)}
 
 				{item.list && <TimelineItemList list={item.list} isOdd={isOdd} />}
@@ -86,7 +91,7 @@ function TimelineItem({ isOdd, item }: { isOdd: boolean; item: TimelineItem }) {
 							<li key={index}>
 								<a
 									href={button.href}
-									className="text-accent font-bold bg-primary tracking-widest text-sm px-4 py-2 rounded-xl hover:bg-primary-400 transition-colors"
+									className="px-4 py-2 text-sm font-bold tracking-widest transition-colors text-accent bg-primary rounded-xl hover:bg-primary-400"
 								>
 									{button.text}
 								</a>
@@ -142,11 +147,23 @@ function TimelineItemList({
 								{listItem.technologies.map((technology, index) => (
 									<li
 										key={index}
-										className="rounded bg-accent-600 px-1 py-px text-xs font-medium hover:bg-primary transition-colors"
+										className="px-1 py-px text-xs font-medium transition-colors rounded bg-accent-600 hover:bg-primary"
 									>
 										{technology}
 									</li>
 								))}
+								{listItem.link && (
+									<li className="px-1 py-px text-xs font-medium transition-colors rounded bg-primary hover:bg-primary-700">
+										<a
+											href={listItem.link.href}
+											target="_blank"
+											rel="noopener noreferer"
+										>
+											<FontAwesomeIcon icon={faArrowUpRightFromSquare} />{" "}
+											{listItem.link.text}
+										</a>
+									</li>
+								)}
 							</ul>
 						)}
 					</li>
@@ -157,7 +174,9 @@ function TimelineItemList({
 					{...getToggleProps()}
 					className={clsx(
 						"absolute z-20 transition-all text-lg text-accent-400 rounded-full grid place-items-center w-8 h-8 hover:bg-accent-700 bg-transparent",
-						isOdd ? "right-full" : "right-[calc(100%+24px)]",
+						isOdd
+							? "right-[calc(100%+24px)] lg:right-0 lg:left-[calc(100%+24px)]"
+							: "right-[calc(100%+24px)]",
 						isExpanded ? "top-[calc(100%-30px)]" : "top-[calc(100%-30px)]"
 					)}
 				>
